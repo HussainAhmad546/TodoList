c39b91d2ea05ad181ab81b340d4083e8b22c8a7c
Syed Hussain Ahmad <113574630+HussainAhmad546@users.noreply.github.com>
2023-09-11 18:30:29 +0500

    To-do App

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb-base', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    quotes: [2, 'single', { avoidEscape: true }],
+    'prettier/prettier': ['error', { singleQuote: true }],
+    semi: ['error', 'always'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'linebreak-style': ['error', 'unix'],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.tsx', '.jsx', '.js'] },
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    // 'no-unused-vars': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
